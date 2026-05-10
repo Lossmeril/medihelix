2f9c91c30cbff9089db464ffb68676047a1b608e
Michal Špitálský <michal.spitalsky@pm.me>
2026-05-10 10:44:15 +0200

    fix (*): homepage anchors

--- components/sections/homepage/whatWeDoSection.tsx
+++ components/sections/homepage/whatWeDoSection.tsx
@@ -100,7 +100,7 @@ export default function WhatWeDoSection({
   });
 
   return (
-    <Section anchor="co-delame" minHeight="content" borderBottom>
+    <Section anchor="nabidka" minHeight="content" borderBottom>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 items-center">
           <div className="grid grid-cols-1 grid-rows-3 gap-6">
